9c4e51c87bb1bdde5ab3fc9d2b967abc3cd0e811
Erikas Neverdauskas <e.neverdauskas@adform.com>
2018-10-30 15:13:34 +0200

    possibility to toggle place.visited option

--- ui/src/App/BucketList/List/Place/Place.js
+++ ui/src/App/BucketList/List/Place/Place.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
 
-// import updatePlaceMutation from '../../../../graphql/updatePlaceMutation';
+import updatePlaceMutation from '../../../../graphql/updatePlaceMutation';
 import PlaceView from './PlaceView';
 
 const Place = ({ place }) => {
-  return <PlaceView place={place} toggleVisited={() => {}} />;
+  return (
+    <Mutation key={place.id} mutation={updatePlaceMutation}>
+      {updatePlace => {
+        const toggleVisited = id => {
+          updatePlace({
+            variables: {
+              input: { id, visited: !place.visited },
+            },
+          });
+        };
+
+        return <PlaceView place={place} toggleVisited={toggleVisited} />;
+      }}
+    </Mutation>
+  );
 };
 
 export default Place;
